6607f7ddcb1a5c22f057baa159fed9da280b4315
Luiz Aoqui <luiz@hashicorp.com>
2020-04-01 19:38:18 -0400

    add some godoc comments

--- strategy/action.go
+++ strategy/action.go
@@ -3,6 +3,7 @@ package strategy
 import "fmt"
 
 const (
+	// Standarized Meta keys used by the Autoscaler.
 	metaKeyDryRun        = "nomad_autoscaler.dry_run"
 	metaKeyDryRunCount   = "nomad_autoscaler.dry_run.count"
 	metaKeyCountCapped   = "nomad_autoscaler.count.capped"
@@ -11,12 +12,14 @@ const (
 	metaKeyReasonHistory = "nomad_autoscaler.reason_history"
 )
 
+// Action represents a Strategy's intention to modify.
 type Action struct {
 	Count  *int64
 	Reason string
 	Meta   map[string]interface{}
 }
 
+// Canonicalize ensures Action has proper default values.
 func (a *Action) Canonicalize() {
 	if a.Meta == nil {
 		a.Meta = make(map[string]interface{})
@@ -27,6 +30,9 @@ func (a *Action) Canonicalize() {
 	}
 }
 
+// SetDryRun marks the Action to be executed in dry-run mode.
+// Dry-run mode is indicated using Meta tags.
+// A dry-run action doesn't modify the Target's count value.
 func (a *Action) SetDryRun(dryRun bool) {
 	a.Meta[metaKeyDryRun] = true
 	if a.Count != nil {
@@ -35,6 +41,8 @@ func (a *Action) SetDryRun(dryRun bool) {
 	a.Count = nil
 }
 
+// CapCount caps the value of Count so it remains within the specified limits.
+// If Count is nil this method has no effect.
 func (a *Action) CapCount(min, max int64) {
 	if a.Count == nil {
 		return
@@ -55,6 +63,7 @@ func (a *Action) CapCount(min, max int64) {
 	}
 }
 
+// PushReason updates the Reason value and stores previous Reason into Meta.
 func (a *Action) PushReason(r string) {
 	history := []string{}
 

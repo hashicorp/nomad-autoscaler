016827b6e2a0339a15f3e71b22a8b3b7ff9ce9d3
Luiz Aoqui <luiz@hashicorp.com>
2020-04-28 12:00:45 -0400

    remove previous state store implementation

--- helper/blocking/blocking.go
+++ helper/blocking/blocking.go
@@ -3,12 +3,3 @@ package blocking
 // IndexHasChanged is used to check whether a returned blocking query has an
 // updated index, compared to a tracked value.
 func IndexHasChanged(new, old uint64) bool { return new > old }
-
-// FindMaxFound is used to determine which value passed is the greatest. This
-// is used to track the most recently found highest index value.
-func FindMaxFound(new, old uint64) uint64 {
-	if new <= old {
-		return old
-	}
-	return new
-}
--- helper/blocking/blocking_test.go
+++ helper/blocking/blocking_test.go
@@ -34,37 +34,3 @@ func Test_indexHasChange(t *testing.T) {
 		assert.Equal(t, tc.expectedReturn, res)
 	}
 }
-
-func Test_findMaxFound(t *testing.T) {
-	testCases := []struct {
-		newValue       uint64
-		oldValue       uint64
-		expectedReturn uint64
-	}{
-		{
-			newValue:       13,
-			oldValue:       7,
-			expectedReturn: 13,
-		},
-		{
-			newValue:       13696,
-			oldValue:       13696,
-			expectedReturn: 13696,
-		},
-		{
-			newValue:       7,
-			oldValue:       13,
-			expectedReturn: 13,
-		},
-		{
-			newValue:       1,
-			oldValue:       0,
-			expectedReturn: 1,
-		},
-	}
-
-	for _, tc := range testCases {
-		res := FindMaxFound(tc.newValue, tc.oldValue)
-		assert.Equal(t, tc.expectedReturn, res)
-	}
-}

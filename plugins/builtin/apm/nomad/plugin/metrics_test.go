00be9d2ccb11d34608fd61d2ebfb17e2456d783a
James Rasell <jrasell@hashicorp.com>
2020-05-15 09:56:30 +0200

    plugin: fix Nomad APM bug when querying groups on multiple clients.
    The Nomad APM used to /v1/metrics endpoint to gather task group
    mertric data. The endpoint unfortunatly only returns metrics for
    the specific client queried. In clustered setups where a task
    group is running on multiple hosts, this meant data points were
    being missed.
    
    The change moves to using the Jobs().Allocations() and
    Allocations().Stats() endpoints. These are much more inefficient
    than the metrics endpoint, but ensure all task group data points
    can be captured no matter their location on a cluster.

--- plugins/builtin/apm/nomad/plugin/metrics_test.go
+++ plugins/builtin/apm/nomad/plugin/metrics_test.go
@@ -6,54 +6,84 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_calculateResult(t *testing.T) {
+	testCases := []struct {
+		inputOp        string
+		inputMetrics   []float64
+		expectedOutput float64
+		name           string
+	}{
+		{
+			inputOp:        queryOpSum,
+			inputMetrics:   []float64{76.34, 13.13, 24.50},
+			expectedOutput: 113.97,
+			name:           "sum operation",
+		},
+		{
+			inputOp:        queryOpAvg,
+			inputMetrics:   []float64{76.34, 13.13, 24.50},
+			expectedOutput: 37.99,
+			name:           "avg operation",
+		},
+		{
+			inputOp:        queryOpMax,
+			inputMetrics:   []float64{76.34, 13.13, 24.50, 76.33},
+			expectedOutput: 76.34,
+			name:           "max operation",
+		},
+		{
+			inputOp:        queryOpMin,
+			inputMetrics:   []float64{76.34, 13.13, 24.50, 13.14},
+			expectedOutput: 13.13,
+			name:           "min operation",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actualOutput := calculateResult(tc.inputOp, tc.inputMetrics)
+			assert.Equal(t, tc.expectedOutput, actualOutput, tc.name)
+		})
+	}
+}
+
 func Test_parseQuery(t *testing.T) {
 	testCases := []struct {
 		name        string
 		input       string
-		expected    *Query
+		expected    *query
 		expectError bool
 	}{
 		{
 			name:  "avg_cpu",
 			input: "avg_cpu/group/job",
-			expected: &Query{
-				Metric:    "nomad.client.allocs.cpu.total_percent",
-				Job:       "job",
-				Group:     "group",
-				Operation: "avg",
+			expected: &query{
+				metric:    "cpu",
+				job:       "job",
+				group:     "group",
+				operation: "avg",
 			},
 			expectError: false,
 		},
 		{
 			name:  "avg_memory",
 			input: "avg_memory/group/job",
-			expected: &Query{
-				Metric:    "nomad.client.allocs.memory.usage",
-				Job:       "job",
-				Group:     "group",
-				Operation: "avg",
-			},
-			expectError: false,
-		},
-		{
-			name:  "arbritary metric",
-			input: "avg_nomad.client.allocs.cpu.total_percent/group/job",
-			expected: &Query{
-				Metric:    "nomad.client.allocs.cpu.total_percent",
-				Job:       "job",
-				Group:     "group",
-				Operation: "avg",
+			expected: &query{
+				metric:    "memory",
+				job:       "job",
+				group:     "group",
+				operation: "avg",
 			},
 			expectError: false,
 		},
 		{
 			name:  "job with fwd slashes",
 			input: "avg_cpu/group/my/super/job//",
-			expected: &Query{
-				Metric:    "nomad.client.allocs.cpu.total_percent",
-				Job:       "my/super/job//",
-				Group:     "group",
-				Operation: "avg",
+			expected: &query{
+				metric:    "cpu",
+				job:       "my/super/job//",
+				group:     "group",
+				operation: "avg",
 			},
 			expectError: false,
 		},

391da66f3332e23b5dc9a4b99d1b5c4f43f93774
James Rasell <jrasell@hashicorp.com>
2020-09-15 16:49:20 +0200

    sdk: move ScalingCheckEvaluation into its own file.

--- sdk/eval.go
+++ sdk/eval.go
@@ -8,22 +8,6 @@ type ScalingEvaluation struct {
 	CheckEvaluations []*ScalingCheckEvaluation
 }
 
-// ScalingCheckEvaluation is the evaluation of an individual policy check. Each
-// check eval within a ScalingEvaluation is performed concurrently and a single
-// "winner" picked once all have returned.
-type ScalingCheckEvaluation struct {
-
-	// Check is the individual ScalingPolicyCheck that this eval is concerned
-	// with.
-	Check *ScalingPolicyCheck
-
-	// Metric is the metric resulting from querying the APM.
-	Metric float64
-
-	// Action is the calculated desired state and is populated by strategy.Run.
-	Action *ScalingAction
-}
-
 // NewScalingEvaluation creates a new ScalingEvaluation based off the passed
 // policy and status. It is responsible for hydrating all the fields to a basic
 // level for safe usage throughout the scaling evaluation phase.

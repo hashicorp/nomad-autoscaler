ea7dab724204de8da7e3faf413d27c88b232d8f9
James Rasell <jrasell@users.noreply.github.com>
2020-09-21 10:16:26 +0200

    sdk: move ScalingCheckEvaluation into its own file. (#269)

---
+++ sdk/eval_oss.go
@@ -0,0 +1,19 @@
+// +build !ent
+
+package sdk
+
+// ScalingCheckEvaluation is the evaluation of an individual policy check. Each
+// check eval within a ScalingEvaluation is performed concurrently and a single
+// "winner" picked once all have returned.
+type ScalingCheckEvaluation struct {
+
+	// Check is the individual ScalingPolicyCheck that this eval is concerned
+	// with.
+	Check *ScalingPolicyCheck
+
+	// Metrics is the metric resulting from querying the APM.
+	Metrics TimestampedMetrics
+
+	// Action is the calculated desired state and is populated by strategy.Run.
+	Action *ScalingAction
+}

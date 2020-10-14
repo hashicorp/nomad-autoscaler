7c29407a15c7c2ce1736f03374257969d2e62638
James Rasell <jrasell@hashicorp.com>
2020-10-14 08:40:01 +0200

    plugins: add enterprise plugin management option and stub funcs.

---
+++ plugins/manager/internal_oss.go
@@ -0,0 +1,8 @@
+// +build !ent
+
+package manager
+
+import "github.com/hashicorp/nomad-autoscaler/agent/config"
+
+func isEnterprise(_ string) bool                                          { return false }
+func (pm *PluginManager) loadEnterprisePlugin(_ *config.Plugin, _ string) {}

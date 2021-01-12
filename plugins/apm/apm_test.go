f033f5ca3ab155fd780a35caa6f493250019471e
Luiz Aoqui <luiz@hashicorp.com>
2021-01-12 00:43:59 -0500

    attempt to test external plugins

---
+++ plugins/apm/apm_test.go
@@ -0,0 +1,71 @@
+package apm
+
+import (
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/go-plugin"
+	"github.com/hashicorp/nomad-autoscaler/sdk"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TODO(luiz): there's an import cycle, so let's copy it here for now.
+var handshake = plugin.HandshakeConfig{
+	ProtocolVersion:  1,
+	MagicCookieKey:   "NOMAD_AUTOSCALER_PLUGIN_MAGIC_COOKIE",
+	MagicCookieValue: "e082fa04d587a6525d683666fa253d6afda00f20c122c54a80a3ed57fec99ff3",
+}
+
+func TestRPCServerQuery(t *testing.T) {
+	client := plugin.NewClient(&plugin.ClientConfig{
+		HandshakeConfig: handshake,
+		Plugins:         map[string]plugin.Plugin{"apm": &Plugin{}},
+		Cmd:             exec.Command("../test/bin/noop-apm"),
+	})
+	defer client.Kill()
+
+	rpcClient, err := client.Client()
+	defer rpcClient.Close()
+	require.NoError(t, err)
+
+	raw, err := rpcClient.Dispense("apm")
+	require.NoError(t, err)
+	apmImpl := raw.(APM)
+
+	now := time.Now()
+	r := sdk.TimeRange{From: now.Add(-10 * time.Second), To: now}
+
+	result, err := apmImpl.Query("fixed:5", r)
+	require.NoError(t, err)
+	assert.Len(t, result, 10)
+}
+
+func TestRPCServerQueryMultiple(t *testing.T) {
+	client := plugin.NewClient(&plugin.ClientConfig{
+		HandshakeConfig: handshake,
+		Plugins:         map[string]plugin.Plugin{"apm": &Plugin{}},
+		Cmd:             exec.Command("../test/bin/noop-apm"),
+		//Cmd: exec.Command("../../bin/plugins/nomad-apm"),
+	})
+	defer client.Kill()
+
+	rpcClient, err := client.Client()
+	require.NoError(t, err)
+
+	raw, err := rpcClient.Dispense("apm")
+	require.NoError(t, err)
+	apmImpl := raw.(APM)
+
+	now := time.Now()
+	r := sdk.TimeRange{From: now.Add(-10 * time.Second), To: now}
+
+	apmImpl.SetConfig(map[string]string{})
+
+	result, err := apmImpl.QueryMultiple("fixed:5", r)
+	//result, err := apmImpl.QueryMultiple("taskgroup_avg_cpu/cache/example", r)
+	require.NoError(t, err)
+	assert.Len(t, result, 1)
+	assert.Len(t, result[0], 1)
+}

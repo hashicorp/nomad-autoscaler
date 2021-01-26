3d5dc01ee8ffff65a63c3b89878c12a511f79dc1
James Rasell <jrasell@hashicorp.com>
2021-01-26 14:55:19 +0100

    plugins/target: use gRPC for Target plugins.

---
+++ plugins/target/client.go
@@ -0,0 +1,48 @@
+package target
+
+import (
+	"context"
+
+	"github.com/hashicorp/nomad-autoscaler/plugins/base"
+	"github.com/hashicorp/nomad-autoscaler/plugins/shared"
+	"github.com/hashicorp/nomad-autoscaler/plugins/target/proto/v1"
+	"github.com/hashicorp/nomad-autoscaler/sdk"
+)
+
+// pluginClient is the gRPC client implementation of the Target interface.
+type pluginClient struct {
+
+	// Embed the base plugin client so that the Target plugin implements the
+	// base interface.
+	*base.PluginClient
+
+	client  proto.TargetPluginServiceClient
+	doneCTX context.Context
+}
+
+// Scale is the gRPC client implementation of the Target.Scale interface
+// function.
+func (p *pluginClient) Scale(action sdk.ScalingAction, config map[string]string) error {
+	req, err := shared.ScalingActionToProto(action)
+	if err != nil {
+		return err
+	}
+	_, err = p.client.Scale(p.doneCTX, &proto.ScaleRequest{Action: req, Config: config})
+	return err
+}
+
+// Status is the gRPC client implementation of the Target.Status interface
+// function.
+func (p *pluginClient) Status(config map[string]string) (*sdk.TargetStatus, error) {
+
+	statusResp, err := p.client.Status(p.doneCTX, &proto.StatusRequest{Config: config})
+	if err != nil {
+		return nil, err
+	}
+
+	return &sdk.TargetStatus{
+		Ready: statusResp.Ready,
+		Count: statusResp.Count,
+		Meta:  statusResp.Meta,
+	}, nil
+}
--- plugins/target/target.go
+++ plugins/target/target.go
@@ -1,105 +1,26 @@
 package target
 
 import (
-	"net/rpc"
-
-	plugin "github.com/hashicorp/go-plugin"
 	"github.com/hashicorp/nomad-autoscaler/plugins/base"
 	"github.com/hashicorp/nomad-autoscaler/sdk"
 )
 
+// Target is the interface that all Target plugins are required to implement.
+// The plugins are responsible for providing status details of the remote
+// target, as well as carrying out scaling actions as decided by the Strategy
+// plugin and internal autoscaler controls.
 type Target interface {
+
+	// Embed base.Base ensuring that strategy plugins implement this interface.
+	base.Base
+
+	// Scale triggers a scaling action against the remote target as specified
+	// by the config func argument.
 	Scale(action sdk.ScalingAction, config map[string]string) error
+
+	// Status collects and returns critical information of the status of the
+	// remote target. The information is used to understand whether the target
+	// is in a position to be scaled as well as the current running count which
+	// will be used when performing the strategy calculation.
 	Status(config map[string]string) (*sdk.TargetStatus, error)
-	PluginInfo() (*base.PluginInfo, error)
-	SetConfig(config map[string]string) error
-}
-
-// RPC is a plugin implementation that talks over net/rpc
-type RPC struct {
-	client *rpc.Client
-}
-
-type RPCScaleRequest struct {
-	Action sdk.ScalingAction
-	Config map[string]string
-}
-
-func (r *RPC) SetConfig(config map[string]string) error {
-	var resp error
-	err := r.client.Call("Plugin.SetConfig", config, &resp)
-	if err != nil {
-		return err
-	}
-	return resp
-}
-
-func (r *RPC) PluginInfo() (*base.PluginInfo, error) {
-	var resp base.PluginInfo
-	err := r.client.Call("Plugin.PluginInfo", new(interface{}), &resp)
-	return &resp, err
-}
-
-func (r *RPC) Status(config map[string]string) (*sdk.TargetStatus, error) {
-	var resp sdk.TargetStatus
-	err := r.client.Call("Plugin.Status", config, &resp)
-	return &resp, err
-}
-
-func (r *RPC) Scale(action sdk.ScalingAction, config map[string]string) error {
-	var resp error
-	req := RPCScaleRequest{
-		Action: action,
-		Config: config,
-	}
-	err := r.client.Call("Plugin.Scale", req, &resp)
-	if err != nil {
-		return err
-	}
-	return resp
-}
-
-// RPCServer is the net/rpc server
-type RPCServer struct {
-	Impl Target
-}
-
-func (s *RPCServer) SetConfig(config map[string]string, resp *error) error {
-	err := s.Impl.SetConfig(config)
-	*resp = err
-	return err
-}
-
-func (s *RPCServer) PluginInfo(_ interface{}, r *base.PluginInfo) error {
-	resp, err := s.Impl.PluginInfo()
-	if resp != nil {
-		*r = *resp
-	}
-	return err
-}
-
-func (s *RPCServer) Status(config map[string]string, resp *sdk.TargetStatus) error {
-	status, err := s.Impl.Status(config)
-	if status != nil {
-		*resp = *status
-	}
-	return err
-}
-
-func (s *RPCServer) Scale(req RPCScaleRequest, resp *error) error {
-	err := s.Impl.Scale(req.Action, req.Config)
-	return err
-}
-
-// Plugin is the plugin.Plugin
-type Plugin struct {
-	Impl Target
-}
-
-func (p *Plugin) Server(*plugin.MuxBroker) (interface{}, error) {
-	return &RPCServer{Impl: p.Impl}, nil
-}
-
-func (Plugin) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
-	return &RPC{client: c}, nil
 }

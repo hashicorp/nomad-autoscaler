0d869b013ba781090b25e38e8fb13c6b1d218116
James Rasell <jrasell@hashicorp.com>
2021-04-27 15:06:32 +0200

    HA: add base hash functionality to form ring.

---
+++ sdk/helper/hash/hash.go
@@ -0,0 +1,22 @@
+package hash
+
+import "github.com/cespare/xxhash"
+
+// Hasher is responsible for generating unsigned, 64 bit hash of provided byte
+// array.
+type Hasher interface {
+
+	// Hash64 takes an input byte array and returns a hashed version.
+	Hash64([]byte) uint64
+}
+
+// xxHasher is the xxhash 64-bit implementation of the Hasher interface and
+// generates hashes according to http://cyan4973.github.io/xxHash/.
+type xxHasher struct{}
+
+// NewDefaultHasher returns a new xxhasher implementation of the Hasher
+// interface.
+func NewDefaultHasher() Hasher { return xxHasher{} }
+
+// Hash64 satisfies the Hash64 function on the Hasher interface.
+func (x xxHasher) Hash64(key []byte) uint64 { return xxhash.Sum64(key) }

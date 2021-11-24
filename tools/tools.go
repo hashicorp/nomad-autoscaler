05a75829aa6e77711afd2300da856733393c503e
Luiz Aoqui <luiz@hashicorp.com>
2021-11-23 19:31:38 -0500

    gofmt files after updating to Go 1.17

--- tools/tools.go
+++ tools/tools.go
@@ -1,4 +1,5 @@
-//+build tools
+//go:build tools
+// +build tools
 
 // Package tools anonymously imports packages of tools used to build, test and
 // lint Nomad Autoscaler. See the GNUMakefile for `go get` commands.

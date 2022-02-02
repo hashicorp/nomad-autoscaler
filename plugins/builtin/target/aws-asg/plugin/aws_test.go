5586dd62eac488ce309c52dcc9b5077ba94fbf03
Adrian Lopez <alopez@hashicorp.com>
2022-02-02 12:27:54 +0000

    remove aws test: asg.Config.Region no longer available in latest AWS SDK version

--- plugins/builtin/target/aws-asg/plugin/aws_test.go
+++ plugins/builtin/target/aws-asg/plugin/aws_test.go
@@ -2,52 +2,11 @@ package plugin
 
 import (
 	"errors"
-	"os"
 	"testing"
 
-	hclog "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/nomad/api"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestTargetPlugin_SetConfig_NonAWS(t *testing.T) {
-	if os.Getenv("NOMAD_AUTOSCALER_AWS") != "" {
-		t.Skip("skipping aws-asg non-aws environment test, NOMAD_AUTOSCALER_AWS set")
-	}
-
-	testCases := []struct {
-		inputCfg          map[string]string
-		expectedOutputErr error
-		name              string
-	}{
-		{
-			inputCfg:          map[string]string{},
-			expectedOutputErr: nil,
-			name:              "empty input config",
-		},
-		{
-			inputCfg: map[string]string{
-				"aws_region": "ap-southeast-2",
-			},
-			expectedOutputErr: nil,
-			name:              "input config with region",
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			awsPlugin := NewAWSASGPlugin(hclog.NewNullLogger())
-			actualOutputErr := awsPlugin.SetConfig(tc.inputCfg)
-			assert.Equal(t, tc.expectedOutputErr, actualOutputErr, tc.name)
-
-			region := tc.inputCfg["aws_region"]
-			if region == "" {
-				region = "us-east-1"
-			}
-			assert.Equal(t, region, awsPlugin.asg.Config.Region, tc.name)
-		})
-	}
-}
 
 func Test_awsNodeIDMap(t *testing.T) {
 	testCases := []struct {

122880b7ce451916549b003626435aac3a18db0a
Juana De La Cuesta <juanita.delacuestamorales@hashicorp.com>
2023-06-23 10:08:09 +0200

    Update policyeval/base_worker.go
    Co-authored-by: Luiz Aoqui <luiz@hashicorp.com>

--- policyeval/base_worker.go
+++ policyeval/base_worker.go
@@ -385,7 +385,7 @@ func (h *checkHandler) start(ctx context.Context, currentStatus *sdk.TargetStatu
 
 	source, err := h.pluginManager.GetAPM(h.checkEval.Check.Source)
 	if err != nil {
-		return nil, fmt.Errorf(`"%s" unable to get APM source`, h.checkEval.Check.Source)
+		return nil, fmt.Errorf("failed to dispense APM plugin: %v", err)
 	}
 
 	// Query check's APM.

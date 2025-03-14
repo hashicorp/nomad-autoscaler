ceb54595b417592fc589513c1452e6bf743f0a95
Allan Clark <allanc@chickenandpork.com>
2025-03-13 23:12:24 -0700

    fix: better identifiers, error strings, per @schmichael

--- plugins/builtin/target/ibmcloud-ig/plugin/plugin.go
+++ plugins/builtin/target/ibmcloud-ig/plugin/plugin.go
@@ -36,7 +36,7 @@ var (
 type IBMCloudIG struct {
 	config map[string]string
 	logger hclog.Logger
-	vpc *vpcv1.VpcV1
+	vpc    *vpcv1.VpcV1
 }
 
 // NewAWSASGPlugin returns the IBMCloud Instance Group implementation of the target.Target
@@ -56,32 +56,32 @@ func (n *IBMCloudIG) Scale(action sdk.ScalingAction, config map[string]string) e
 	}
 
 	// We cannot get the instance group from the name, we only have an API to get from ID
-	instance_group_id, ok := n.getConfig(configKeyInstanceGroupID, config)
+	instanceGroupID, ok := n.getConfig(configKeyInstanceGroupID, config)
 	if !ok {
 		return fmt.Errorf("required config param %s not found", configKeyInstanceGroupID)
 	}
 
-	api_key, ok := n.getConfig(configKeyAPIKey, config)
+	apiKey, ok := n.getConfig(configKeyAPIKey, config)
 	if !ok {
 		return fmt.Errorf("required config param %s not found", configKeyAPIKey)
 	}
 
-	// ensure we have a lazy-defined client handle
+	// ensure we have an authenticated lazy-defined client handle
 	if n.vpc == nil {
 		vpc, verr := vpcv1.NewVpcV1(
 			&vpcv1.VpcV1Options{
-				Authenticator: &core.IamAuthenticator{ApiKey: api_key},
+				Authenticator: &core.IamAuthenticator{ApiKey: apiKey},
 			})
 		if verr != nil {
-			return fmt.Errorf("failed to construct VPC instance: %w", verr)
+			return fmt.Errorf("error building authenticated client handle: %w", verr)
 		}
 		n.vpc = vpc
 	}
 
 	// hit that client handle to query information about the victim Instance Group
-	ig, _, err := n.vpc.GetInstanceGroup(&vpcv1.GetInstanceGroupOptions{ID: &instance_group_id})
+	ig, _, err := n.vpc.GetInstanceGroup(&vpcv1.GetInstanceGroupOptions{ID: &instanceGroupID})
 	if err != nil {
-		fmt.Errorf("IG: %s: Exception in GetInstanceGroup: %v\n", instance_group_id, err)
+		return fmt.Errorf("failed to GetInstanceGroup for ID %s: %w", instanceGroupID, err)
 	}
 
 	n.logger.Info(
@@ -96,7 +96,7 @@ func (n *IBMCloudIG) Scale(action sdk.ScalingAction, config map[string]string) e
 	instanceGroupPatchModel.MembershipCount = core.Int64Ptr(int64(action.Count))
 	instanceGroupPatch, err := instanceGroupPatchModel.AsPatch()
 	if err != nil {
-		return fmt.Errorf("IG: %s: Exception in instanceGroupPatchModel: %v\n", instance_group_id, err)
+		return fmt.Errorf("error creating patch for instance group %s: %w", instanceGroupID, err)
 	}
 
 	// Apply the patch
@@ -107,14 +107,14 @@ func (n *IBMCloudIG) Scale(action sdk.ScalingAction, config map[string]string) e
 	// instanceGroup, response, err := vpc.UpdateInstanceGroup(...)
 	_, _, uerr := n.vpc.UpdateInstanceGroup(options)
 	if uerr != nil {
-		return fmt.Errorf("IG: %s: Exception in UpdateInstanceGroup: %v\n", instance_group_id, uerr)
+		return fmt.Errorf("error updating Instance Group with ID %s: %w", instanceGroupID, uerr)
 	}
 
 	// Victory Lap -- Maybe this can be removed to cut traffic to logfiles
 	n.logger.Info(
 		fmt.Sprintf(
-			"ASG %s (%s) was scaled to %d using the IBM Cloud API\n",
-			instance_group_id, *ig.Name, action.Count,
+			"ASG %s (%s) was scaled to %d using the IBM Cloud API",
+			instanceGroupID, *ig.Name, action.Count,
 		),
 	)
 
@@ -122,12 +122,12 @@ func (n *IBMCloudIG) Scale(action sdk.ScalingAction, config map[string]string) e
 }
 
 func (n *IBMCloudIG) Status(config map[string]string) (*sdk.TargetStatus, error) {
-	instance_group_id, ok := n.getConfig(configKeyInstanceGroupID, config)
+	instanceGroupID, ok := n.getConfig(configKeyInstanceGroupID, config)
 	if !ok {
 		return nil, fmt.Errorf("required config param %s not found", configKeyInstanceGroupID)
 	}
 
-	api_key, ok := n.getConfig(configKeyAPIKey, config)
+	apiKey, ok := n.getConfig(configKeyAPIKey, config)
 	if !ok {
 		return nil, fmt.Errorf("required config param %s not found", configKeyAPIKey)
 	}
@@ -136,18 +136,18 @@ func (n *IBMCloudIG) Status(config map[string]string) (*sdk.TargetStatus, error)
 	if n.vpc == nil {
 		vpc, verr := vpcv1.NewVpcV1(
 			&vpcv1.VpcV1Options{
-				Authenticator: &core.IamAuthenticator{ApiKey: api_key},
+				Authenticator: &core.IamAuthenticator{ApiKey: apiKey},
 			})
 		if verr != nil {
-			return nil, fmt.Errorf("Exception in NewVpcV1UsingExternalConfig: %v\n", verr)
+			return nil, fmt.Errorf("error building authenticated client handle: %w", verr)
 		}
 		n.vpc = vpc
 	}
 
 	// hit that client handle to query information about the victim Instance Group
-	ig, _, err := n.vpc.GetInstanceGroup(&vpcv1.GetInstanceGroupOptions{ID: &instance_group_id})
+	ig, _, err := n.vpc.GetInstanceGroup(&vpcv1.GetInstanceGroupOptions{ID: &instanceGroupID})
 	if err != nil {
-		return nil, fmt.Errorf("IG: %s: Exception in GetInstanceGroup: %v\n", instance_group_id, err)
+		return nil, fmt.Errorf("failed to GetInstanceGroup for ID %s: %w", instanceGroupID, err)
 	}
 
 	n.logger.Info(
